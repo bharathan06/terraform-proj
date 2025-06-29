5b8d9619c512939ced3d44201f95ab096916abba
bharathan06 <bharathan.m.blr@gmail.com>
2025-06-29 18:50:39 +0530

    compute module is completed, tested with validate and plan seems good

--- azure-3tier/envs/dev/main.tf
+++ azure-3tier/envs/dev/main.tf
@@ -11,3 +11,22 @@ module "network" {
   resource_group      = var.resource_group
   tags                = var.tags
 }
+
+
+module "compute" {
+  source        = "../../modules/compute"
+  resource_group = var.resource_group
+  location       = var.location
+  subnet_ids     = [ module.network.public_subnet_id, module.network.private_subnet_id ]
+
+  instance_count  = var.instance_count
+  vm_size         = var.vm_size
+  admin_username  = var.admin_username
+  admin_password  = var.admin_password
+
+  image_publisher = var.image_publisher
+  image_offer     = var.image_offer
+  image_sku       = var.image_sku
+
+  tags            = var.tags
+}
--- azure-3tier/envs/dev/variables.tf
+++ azure-3tier/envs/dev/variables.tf
@@ -1,3 +1,5 @@
+//network variables :  
+
 variable "resource_group" {
   description = "The name of the existing RG to deploy into"
   type        = string
@@ -36,4 +38,38 @@ variable "subscription_id" {
 variable "tenant_id" {
   description = "Azure Tenant (Directory) ID"
   type        = string
+}
+
+
+//Compute variables :
+
+variable "admin_username" {
+  type = string 
+  description = "Admin username for the VMs"
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "instance_count" {
+  type    = number
+  default = 2
+}
+
+variable "vm_size" {
+  type    = string
+}
+
+variable "image_publisher" {
+  type = string
+}
+
+variable "image_offer" {
+  type = string
+}
+
+variable "image_sku" {
+  type = string
 }
